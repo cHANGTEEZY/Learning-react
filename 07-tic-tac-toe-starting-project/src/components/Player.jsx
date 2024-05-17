8a6514a691eefe6ba5c77e8da8fad876be1ae837
Sushank Gurung <jagatgurung30@gmail.com>
2024-05-17 11:49:06 +0545

    react tic tac toe

---
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+export default function Player({ initialName, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(initialName);
+  function handleSave() {
+    setIsEditing((editing) => !editing);
+  }
+
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+  let buttonCaption = "Edit";
+  if (isEditing) {
+    editablePlayerName = (
+      <input required defaultValue={playerName} onChange={handleChange} />
+    );
+    buttonCaption = "Save";
+  }
+
+  return (
+    <li>
+      <span className="player">
+        {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleSave}>{buttonCaption}</button>
+    </li>
+  );
+}
